6223bfdb73856b25aeb7dd9525f201967c2d41a6
Eb-Zeero <macebelene@gmail.com>
2019-06-10 16:13:20 +0200

    creating and removing a datafile

--- util/get_information.py
+++ util/get_information.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-import pymysql
-
 from connection import sdb_connect, ssda_connect
 import pandas as pd
 
@@ -23,6 +21,38 @@ def get_telescope_name(telescope_id):
     return
 
 
+def get_target_id(target_name, right_ascension, declination):
+    sql = """
+    SELECT targetId FROM BlockVisit WHERE name=%s AND rightAscension=%s AND declination=%s
+"""
+
+    df = pd.read_sql(sql, con=sdb_connect(), params=(target_name, right_ascension, declination))
+
+    if df.empty:
+        return None
+
+    return int(df['targetId'][0])
+
+
+def get_data_file_id(filename):
+    """
+
+    :param filename:
+    :return: datafile Id
+    """
+
+    sql = """
+                SELECT dataFileId FROM DataFile WHERE name=%s
+            """
+
+    df = pd.read_sql(sql, con=sdb_connect(), params=(filename))
+
+    if df.empty:
+        return None
+
+    return int(df['dataFileId'][0])
+
+
 def get_target_type_id(block_id):
     """
     Get the target type id from SSDA.
@@ -30,7 +60,8 @@ def get_target_type_id(block_id):
     :param block_id: SBD block id
     :return: target type id.
     """
-
+    if block_id is None:
+        return None
     sdb_target_type_query = """
 SELECT NumericCode from Pointing as p
     JOIN Observation USING(Pointing_Id)
@@ -95,7 +126,7 @@ def get_data_category_id(data_category_name):
     return int(df['dataCategoryId'][0])
 
 
-def get_ssda_observation_id(teleObsID, teleID):
+def get_observation_id(teleObsID, teleID):
     """
     Retrieves the observation id
     
@@ -368,14 +399,16 @@ def populate_target_type():
 def create_data_file(data_category_id, observation_date, filename, target_id, observation_id, file_size=None):
     """
     Create a data file for SSDA
-    :param data_category_id:
-    :param observation_date:
-    :param filename:
-    :param target_id:
-    :param observation_id:
-    :param file_size:
-    :return:
+    :param data_category_id: The data category id. This links to the DataCategory table.
+    :param observation_date: The time when the data taking started, if the data set is a FITS file.
+    :param filename: Name of the file, which must be unique.
+    :param target_id: The id of the target which was observed. This links to the Target table.
+    :param observation_id: The id of the observation to which the data set belongs.
+    :param file_size: The file size in bytes.
+    :return: None
     """
+    conn = ssda_connect()
+    cursor = conn.cursor()
     data_files_sql = """
 INSERT INTO DataFile(
     dataCategoryId,
@@ -396,3 +429,87 @@ VALUES (%s,%s,%s,%s,%s,%s)
         file_size,
         observation_id
     )
+    cursor.execute(data_files_sql, data_files_params)
+    conn.commit()
+
+
+def get_ssda_observation_id(telescope_id, telescope_observation_id):
+    """
+    Retrieves the observation_id
+
+    :return: int or None if does not exist
+    """
+
+    if telescope_observation_id is None:
+        sql = """
+        SELECT observationId FROM Observation ORDER BY observationId DESC LIMIT 1
+            """
+        df = pd.read_sql(sql=sql, con=ssda_connect())
+    else:
+        params = (telescope_observation_id, telescope_id)
+        sql = """
+            SELECT observationId FROM Observation WHERE telescopeObservationId=%s AND telescopeId=%s ORDER
+            BY observationId DESC LIMIT 1
+        """
+
+        df = pd.read_sql(sql=sql, con=ssda_connect(), params=params)
+
+    if df.empty:
+        return None
+
+    return int(df['observationId'][0])
+
+
+def create_data_preview(name, datafile_id, order="DESC"):
+    """
+
+    :param name: Path to the file
+    :param datafile_id: Identifier of the data file to which this preview belongs.
+    :param order: Defines an order within multiple preview files for the same data file
+    :return: None
+    """
+    conn = ssda_connect()
+    cursor = conn.cursor()
+    data_preview_sql = """
+INSERT INTO DataPreview(
+        name,
+        dataFileId,
+        orders
+    )
+    VALUES (%s,%s,%s)
+"""
+    data_preview_params = (
+        name,
+        datafile_id,
+        order
+    )
+    cursor.execute(data_preview_sql, data_preview_params)
+
+
+def remove_file_if_exist(filename):
+
+    conn = ssda_connect()
+    cursor = conn.cursor()
+
+    get_file_data_sql = """
+SELECT dataFileId FROM DataFile WHERE name=%s
+    """
+    df = pd.read_sql(sql=get_file_data_sql, con=ssda_connect(), params=(filename,))
+
+    if df.empty:
+        return None
+
+    data_file_id = int(df['dataFileId'][0])
+
+    remove_data_file_sql = """
+DELETE FROM DataFile WHERE dataFileId=%s
+    """
+    cursor.execute(sql=remove_data_file_sql, con=ssda_connect(), params=(data_file_id,))
+
+    remove_data_preview_sql = """
+    DELETE FROM DataPreview WHERE dataFileId=%s
+        """
+    cursor.execute(sql=remove_data_preview_sql, con=ssda_connect(), params=(data_file_id,))
+
+    conn.commit()
+
