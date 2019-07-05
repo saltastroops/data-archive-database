f3f9744f175988f84e3d216d723bf930c88ea073
Christian Hettlage <hettlage@saao.ac.za>
2019-07-05 17:21:01 +0200

    fixed directory path for Salticam FITS files

--- ssda/instrument/salticam/salticam_fits_data.py
+++ ssda/instrument/salticam/salticam_fits_data.py
@@ -85,7 +85,7 @@ class SalticamFitsData(InstrumentFitsData):
 
         """
 
-        data_directory = "{base_dir}/salt/data/{year}/{monthday}/salticam/raw".format(
+        data_directory = "{base_dir}/salt/data/{year}/{monthday}/scam/raw".format(
             base_dir=os.environ["FITS_BASE_DIR"],
             year=night.strftime("%Y"),
             monthday=night.strftime("%m%d"),
