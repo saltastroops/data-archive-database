6017d4be113ace882fb0cd1ad10e44817f3fb729
Christian Hettlage <hettlage@saao.ac.za>
2019-08-30 17:44:10 +0200

    create installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssda',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires=">=3.7",
+    setup_requires=['pytest-runner'],
+    install_requires=[],
+    tests_require=['pytest'],
+    entry_points={"console_scripts": ["ssda = ssda.cli:main"]},
+    version="0.1.0"
+)
