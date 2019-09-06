0f98f77be999183d0af304d461fa697af125662e
Christian Hettlage <hettlage@saao.ac.za>
2019-09-06 23:20:47 +0200

    abstract base class for observation properties

--- src/ssda/observation.py
+++ src/ssda/observation.py
@@ -1,5 +1,48 @@
-class ObservationProperties:
-    pass
+from abc import ABC
+from typing import List, Optional
+
+from ssda.util import types
+
+
+class ObservationProperties(ABC):
+    """
+    Properties of an observation.
+
+    The methods of this class return objects that correspond to database tables to be
+    populated.
+
+    """
+
+    def artifact(self, plane_id: int) -> types.Artifact:
+        raise NotImplemented
+
+    def energy(self, plane_id: int) -> types.Energy:
+        raise NotImplemented
+
+    def instrument_keyword_values(self, observation_id) -> List[types.InstrumentKeywordValue]:
+        raise NotImplemented
+
+    def observation(self, proposal_id: Optional[int]) -> types.Observation:
+        raise NotImplemented
+
+    def observation_time(self, plane_id: int) -> types.ObservationTime:
+        raise NotImplemented
+
+    def plane(self, observation_id: int) -> types.Plane:
+        raise NotImplemented
+
+    def polarizations(self, plane_id: int) -> List[types.Polarization]:
+        raise NotImplemented
+
+    def position(self, plane_id: int) -> types.Position:
+        raise NotImplemented
+
+    def proposal(self) -> Optional[types.Proposal]:
+        raise NotImplemented
+
+    def target(self) -> types.Target:
+        raise NotImplemented
+
 
 
 class StandardObservationProperties(ObservationProperties):
