0d06080c969ac80f084a168ac3dc0aef8ec16ef4
Eb-Zeero <macebelene@gmail.com>
2020-03-04 13:27:23 +0200

    false

--- sql/V3__admin_tables.sql
+++ sql/V3__admin_tables.sql
@@ -55,8 +55,8 @@ INSERT INTO calibration_type (calibration_type)
 VALUES ('Arc'),
        ('Bias'),
        ('Flat'),
-       ('Radial Velocity Standard'),
-       ('Spectrophotometric Standard');
+       ('Radial velocity standard'),
+       ('Spectrophotometric standard');
 
 -- data_request_status
 
