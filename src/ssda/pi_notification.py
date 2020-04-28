fd274e10f929616be74514cda1f8267be5528af8
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-04-28 08:56:05 +0200

    script to notify the PI when their data is about to be public

--- src/ssda/pi_notification.py
+++ src/ssda/pi_notification.py
@@ -1,7 +1,7 @@
 import os
 import MySQLdb
-import pprint
 import smtplib
+import pprint
 import psycopg2
 import psycopg2.extras
 import datetime
@@ -9,7 +9,7 @@ import click
 
 mail_port = os.environ.get("MAIL_PORT")
 mail_server = os.environ.get("MAIL_SERVER")
-mail_username = os.environ.get("MAIL_USERNAME")
+mail_username = os.environ.get("MAIL_USER")
 mail_password = os.environ.get("MAIL_PASSWORD")
 
 
@@ -46,9 +46,8 @@ def ssda_database_connection():
 date_today = datetime.datetime.now().date()
 
 
-#
-# We get all PIs and their proposals
-def all_pi_and_proposals():
+# We get all PIs and their proposals from sdb
+def all_pi_and_proposals_sdb():
     with sdb_database_connection().cursor(MySQLdb.cursors.DictCursor) as database_connection:
         pi_query = """SELECT Proposal_Code, Email, CONCAT(FirstName," ", Surname) AS FullName
                       FROM ProposalContact AS propCon
@@ -73,9 +72,10 @@ def proposal_release_dates():
         return results
 
 
-def proposals_and_release_dates():
+# We create a dictionary of every PI with each of their proposals and release dates fro each proposal
+def all_release_dates_and_proposals():
     ssda_query_results = proposal_release_dates()
-    sdb_query_results = all_pi_and_proposals()
+    sdb_query_results = all_pi_and_proposals_sdb()
     all_data = dict()
     for pi in sdb_query_results:
         for data in ssda_query_results:
@@ -86,10 +86,29 @@ def proposals_and_release_dates():
     return all_data
 
 
+message = """
+Subject: Release of data
+To: {receiver}
+From: {sender}
+The proposal with proposal code {proposal_code} is due to
+be released soon with its release date being {release_date}.
+Please let us know if you wish to extend the release date. """
+
+sender = "Salt help<salthelp@saao.ac.za>"
+
+
 @click.command()
 @click.option("--public-within", type=int, help="Number of days until release of proposal")
 def send_email(public_within):
-    for key, value in proposals_and_release_dates().items():
-        for proposal, release_date in value["proposals"].items():
-            pass
+    """
+    We loop through the dictionary of all the PIs and their release dates of each
+    proposal and then we send an email to the PI to tell them that their data will
+    be public on the release date in the dictionary for their proposal
+    :param public_within: The number of days until the proposal goes public
+    :return:
+    """
+    for key, value in all_release_dates_and_proposals().items():
+        print(value)
 
+
+send_email()
