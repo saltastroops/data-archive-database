0ab4d44182499e0c4c6771a9573180770ff82b8c
Christian Hettlage <hettlage@saao.ac.za>
2020-04-28 17:38:42 +0200

    "Unknown" value for various lookup tables

---
+++ sql/V6__unknown_values.sql
@@ -0,0 +1,32 @@
+-- "Unknown" data product type
+
+INSERT INTO data_product_type (product_type)
+VALUES ('Unknown');
+
+-- "Unknown" instrument mode
+
+INSERT INTO instrument_mode (instrument_mode)
+VALUES ('Unknown');
+
+-- "Unknown" intent
+
+INSERT INTO intent (intent)
+VALUES ('Unknown');
+
+-- "Unknown" detector mode
+
+INSERT INTO detector_mode (detector_mode)
+VALUES ('Unknown');
+
+-- "Unknown" product category
+
+INSERT INTO product_category (product_category_id, product_category)
+VALUES (7, 'Unknown');
+
+-- "Unknown" product type
+
+WITH product_category_id (id) AS (
+    SELECT product_category_id FROM product_category WHERE product_category.product_category='Unknown'
+)
+INSERT INTO product_type (product_category_id, product_type)
+VALUES((SELECT id FROM product_category_id), 'Unknown');
