73d4385ceac238080e4dda2ec34e6eb118df0ede
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-05-15 09:56:42 +0200

    added the tables when sending email

--- src/ssda/pi_notification.py
+++ src/ssda/pi_notification.py
@@ -5,6 +5,7 @@ import psycopg2
 import psycopg2.extras
 import datetime
 import click
+from prettytable import PrettyTable
 
 
 def sdb_database_connection():
@@ -37,54 +38,66 @@ def ssda_database_connection():
     return ssda_connection
 
 
-date_today = datetime.datetime.now().date()
-
-
-# We get all PIs and their proposals from sdb
-def all_pi_and_proposals_sdb():
-    with sdb_database_connection().cursor(
-        MySQLdb.cursors.DictCursor
-    ) as database_connection:
-        pi_query = """SELECT Proposal_Code, Email, CONCAT(FirstName," ", Surname) AS FullName
-                      FROM ProposalContact AS propCon
-                      JOIN ProposalCode ON ProposalCode.ProposalCode_Id = propCon.ProposalCode_Id
-                      JOIN Investigator ON Investigator.Investigator_Id = propCon.Leader_Id"""
-        database_connection.execute(pi_query)
-        results = database_connection.fetchall()
-        return results
-
-
 # getting the release dates for the proposals in ssda
 def proposal_release_dates():
     with ssda_database_connection().cursor(
-        cursor_factory=psycopg2.extras.DictCursor
+            cursor_factory=psycopg2.extras.DictCursor
     ) as ssda_cursor:
-        psql_query = """SELECT DISTINCT (proposal_code), data_release
-                        FROM observations.proposal prop
-                        JOIN observations.observation obs on prop.proposal_id=obs.proposal_id
-                        WHERE telescope_id=%(telescope_id)s"""
-        ssda_cursor.execute(psql_query, dict(telescope_id=3))
+        psql_query = """SELECT DISTINCT proposal_code, data_release
+                        FROM observations.proposal proposal
+                        JOIN observations.observation obs on proposal.proposal_id=obs.proposal_id
+                        JOIN observations.telescope telescope on obs.telescope_id=telescope.telescope_id
+                        WHERE name=%(telescope_name)s and data_release - CURRENT_DATE BETWEEN 1 and %(days)s"""
+        ssda_cursor.execute(psql_query, dict(telescope_name='SALT', days=189))
         results = ssda_cursor.fetchall()
-        return results
+        ssda_query_results = {proposal["proposal_code"]: proposal["data_release"] for proposal in results}
+        return ssda_query_results
+
+
+# We get all PIs and their proposals from sdb
+def all_pi_and_proposals(proposals):
+    sdb_query_results = {}
+    with sdb_database_connection().cursor(
+            MySQLdb.cursors.DictCursor
+    ) as database_connection:
+        pi_query = """SELECT Proposal_Code, Email, CONCAT(FirstName," ", Surname) AS fullname
+                      FROM ProposalContact AS propCon
+                      JOIN ProposalCode ON ProposalCode.ProposalCode_Id = propCon.ProposalCode_Id
+                      JOIN Investigator ON Investigator.Investigator_Id = propCon.Leader_Id
+                      WHERE Proposal_Code IN %(proposals)s"""
+        database_connection.execute(pi_query, dict(proposals=proposals))
+        results = database_connection.fetchall()
+        for result in results:
+            sdb_query_results[result["Proposal_Code"]] = {
+                "email": result["Email"],
+                "fullname": result["fullname"]
+            }
+        return sdb_query_results
 
 
 # We create a dictionary of every PI with each of their proposals and release dates fro each proposal
 def all_release_dates_and_proposals():
     ssda_query_results = proposal_release_dates()
-    sdb_query_results = all_pi_and_proposals_sdb()
-    all_data = dict()
-    for pi in sdb_query_results:
-        for data in ssda_query_results:
-            if pi["Proposal_Code"] == data["proposal_code"]:
-                if pi["Email"] not in all_data:
-                    all_data[pi["Email"]] = {
-                        "proposals": {},
-                        "FullName": pi["FullName"],
-                    }
-                all_data[pi["Email"]]["proposals"].update(
-                    {pi["Proposal_Code"]: data["data_release"]}
-                )
+    sdb_query_results = all_pi_and_proposals(list(ssda_query_results.keys()))
+
+    all_data = {}
+    for proposal_code in ssda_query_results:
+        if proposal_code not in sdb_query_results:
+            raise ValueError("... some appropriate error message...")
+        email = sdb_query_results[proposal_code]['email']
+        full_name = sdb_query_results[proposal_code]['fullname']
+        release_date = ssda_query_results[proposal_code]
+
+        if email not in all_data:
+            all_data[email] = {
+                "proposal": [],
+                "fullname": full_name,
+            }
+        all_data[email]["proposal"].append({"proposal_code": proposal_code, "release_date": release_date})
     return all_data
+
+
+# pprint.PrettyPrinter(indent=4).pprint(all_release_dates_and_proposals())
 
 
 def text_formatting(proposal):
@@ -96,17 +109,16 @@ def text_formatting(proposal):
         return ", ".join(proposal[:-1]) + ", and " + proposal[-1]
 
 
-message = """
+def sending_email(receiver, information):
+    message = """
 Subject: Release of data
 To: {receiver}
 From: {sender}
-The release date for {proposal_code} is {release_date}.
-Please let us know if you wish to extend the release date. """
 
-sender = "Salt Astronomy Operations<salthelp@saao.ac.za>"
+{data_sent} 
+"""
 
-
-def email_to_be_sent(receiver, proposal, release_date):
+    sender = "salthelp@salt.ac.za"
     mail_port = os.environ.get("MAIL_PORT")
     mail_server = os.environ.get("MAIL_SERVER")
     mail_username = os.environ.get("MAIL_USER")
@@ -120,8 +132,7 @@ def email_to_be_sent(receiver, proposal, release_date):
             message.format(
                 receiver=receiver,
                 sender=sender,
-                proposal_code=proposal,
-                release_date=release_date,
+                data_sent=information
             ),
         )
     return "Email has been sent"
@@ -140,9 +151,7 @@ def read_log_file():
 
 
 @click.command()
-@click.option(
-    "--public-within", type=int, help="Number of days until release of proposal data"
-)
+@click.option("--public-within", type=int, help="Number of days until release of proposal data")
 def send_email(public_within):
     """
     We loop through the dictionary of all the PIs and their release dates of each
@@ -151,31 +160,26 @@ def send_email(public_within):
     :param public_within: The number of days until the proposal goes public
     :return:
     """
-    proposals_to_include = {}
-    for key, value in all_release_dates_and_proposals().items():
-        for proposal, release_date in value["proposals"].items():
-            time_difference = release_date - date_today
-            if (
-                datetime.timedelta(0)
-                <= time_difference
-                <= datetime.timedelta(public_within)
-            ):
-                if key not in proposals_to_include:
-                    proposals_to_include[key] = {proposal: release_date}
-                else:
-                    proposals_to_include[key].update({proposal: release_date})
+    for email, details in all_release_dates_and_proposals().items():
+        x = [prop["proposal_code"] for prop in details["proposal"]]
+        table = PrettyTable()
+        table.field_names = ["Proposal", "Data becomes public"]
+        if len(details["proposal"]) > 1:
+            more_proposals = [proposal["proposal_code"] for proposal in details["proposal"]]
+            more_release_date = [datetime.datetime.strftime(release_date["release_date"], "%Y-%m-%d")
+                                 for release_date in details["proposal"]]
+            for i in range(1, len(more_proposals)):
+                table.add_row([more_proposals[i], more_release_date[i]])
 
-    for email, all_info in proposals_to_include.items():
-        for x, y in all_info.items():
-            file_content = email + " " + x + " " + datetime.datetime.strftime(y, "%Y-%m-%d")
-            if file_content not in read_log_file():
-                log_to_file(email, x, datetime.datetime.strftime(y, "%Y-%m-%d"))
-                all_proposals = text_formatting([proposal for proposal in all_info.keys()])
-                all_dates = text_formatting(
-                    [datetime.datetime.strftime(d, "%Y-%m-%d") for d in list(all_info.values())]
-                )
-                email_to_be_sent(email, all_proposals, all_dates)
-
+        proposals = details["proposal"][0]["proposal_code"]
+        release_date = details["proposal"][0]["release_date"]
+        table.add_row([proposals, release_date])
+        file_content = email + " " + proposals + " " + datetime.datetime.strftime(release_date, "%Y-%m-%d")
+        if file_content not in read_log_file():
+            pass
+            # print(file_content)
+            # sending_email(email, table)
+            # log_to_file(email, proposals, datetime.datetime.strftime(release_date, "%Y-%m-%d"))
     return "Email Sent"
 
 
