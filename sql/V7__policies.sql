f9958c5c59153ab2b977963bfdab2e1e744b2486
Sifiso <myezasifiso11@gmail.com>
2020-06-08 15:23:11 +0200

    Include trigger and policy sql files

--- sql/V7__policies.sql
+++ sql/V7__policies.sql
@@ -2,12 +2,12 @@ SET search_path TO observations, extensions;
 
 -- policy for hiding target coordinates of proprietary observations to not belonging members
 
-ALTER TABLE observations.position ENABLE ROW LEVEL SECURITY;
+ALTER TABLE position ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS position_policy ON observations.position;
+DROP POLICY IF EXISTS position_policy ON position;
 
-CREATE POLICY position_policy ON observations.position
+CREATE POLICY position_policy ON position
 USING (
-    current_setting('my.institution_user_id')::int = ANY(observations.position.institution_member_user_ids)
-    OR observations.position.institution_member_user_ids IS NULL
+    current_setting('my.institution_user_id')::int = ANY(position.institution_member_user_ids)
+    OR position.institution_member_user_ids IS NULL
 );
--- sql/V8__triggers.sql
+++ sql/V8__triggers.sql
@@ -5,18 +5,19 @@ SET search_path TO admin, observations, extensions;
 CREATE OR REPLACE FUNCTION update_proposal_investigator() RETURNS trigger AS
 $BODY$
     BEGIN
-        UPDATE admin.proposal_investigator pi
-        SET pi.institution_member_user_id = NEW.institution_member_user_id
-        FROM observations.proposal p
-        WHERE pi.proposal_id=p.proposal_id AND pi.institution_user_id = NEW.institution_user_id;
+        UPDATE proposal_investigator
+        SET proposal_investigator.institution_member_user_id = NEW.institution_member_user_id
+        FROM observations.proposal
+        WHERE proposal_investigator.proposal_id=proposal.proposal_id
+            AND proposal_investigator.institution_user_id = NEW.institution_user_id;
 
         RETURN NEW;
     END;
 $BODY$ LANGUAGE plpgsql;
 
--- An insert trigger
+-- An insert trigger on the institution user table
 CREATE TRIGGER create_institution_user
 AFTER INSERT
-ON admin.institution_user
+ON institution_user
 FOR EACH ROW
 EXECUTE PROCEDURE update_proposal_investigator();
