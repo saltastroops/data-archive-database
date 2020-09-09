0fab3cfe6a7e7a572e3be211598215a94356d81a
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-09-09 12:18:29 +0200

    dropped columns before dropping table

--- sql/V8__simplified_data_requests.sql
+++ sql/V8__simplified_data_requests.sql
@@ -1,7 +1,9 @@
 SET search_path TO admin;
 
-DROP TABLE data_request_status;
+ALTER TABLE  data_request DROP COLUMN path, DROP COLUMN data_request_status_id;
 
-ALTER TABLE  data_request DROP COLUMN path, data_request_status_id;
+DROP INDEX data_request_status_fk ON data_request;
+
+DROP TABLE data_request_status;
 
 
